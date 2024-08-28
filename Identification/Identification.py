270de32cf4cc9ebae0b084acb81eceed6ea5060e
Brayan Aguirre Ordinola <b.aguirreor@gmail.com>
2024-08-27 23:38:13 -0500

    feat: dtrend

--- Identification/Identification.py
+++ Identification/Identification.py
@@ -1,29 +1,33 @@
-import scipy.io
+from scipy import signal
+from scipy import io
 import numpy as np
 import matplotlib.pyplot as plt
 
 # Cargar el archivo .mat
-datos = scipy.io.loadmat('data.mat')
+datos = io.loadmat('data.mat')
+tiempo = datos['tiempo']
+u = datos['u']
+y = datos['y']
 
-# Imprimir las claves del archivo .mat para ver qué variables están disponibles
-print(datos.keys())
+u1 = signal.detrend(u,0,type = 'constant')
+y1 = signal.detrend(y,0,type = 'constant')
 
-# Supongamos que los datos están en una variable llamada 'data'
-# Puedes acceder a ellos así:
-data = datos['data']  # Reemplaza 'data' con el nombre real de la variable en tu archivo .mat
-
-# Imprimir las primeras filas de los datos para inspeccionar
-print(data)
-
-# Suponiendo que 'data' tiene dos columnas: tiempo y señal
-# Puedes separar las columnas de la siguiente manera:
-tiempo = data[:, 0]  # Primera columna (ej. tiempo)
-senal = data[:, 1]   # Segunda columna (ej. señal)
-
-# Graficar la señal
-plt.plot(tiempo, senal)
-plt.title('Señal Importada del Archivo .mat')
+plt.figure()
+plt.plot(tiempo,u, label='Señal con tendencia')
+plt.plot(tiempo,u1, label='Señal sin tendencia')
+plt.legend()
+plt.title('Eliminación de Tendencia con scipy.signal.detrend')
 plt.xlabel('Tiempo')
-plt.ylabel('Señal')
+plt.ylabel('Amplitud')
+plt.grid(True)
+plt.show()
+
+plt.figure()
+plt.plot(tiempo,y, label='Señal con tendencia')
+plt.plot(tiempo,y1, label='Señal sin tendencia')
+plt.legend()
+plt.title('Eliminación de Tendencia con scipy.signal.detrend')
+plt.xlabel('Tiempo')
+plt.ylabel('Amplitud')
 plt.grid(True)
 plt.show()
